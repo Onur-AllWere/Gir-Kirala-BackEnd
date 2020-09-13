5a12c583044a01e6b3ff30e450f6628e398f18ff
Onur Açan <onur.acan@allwere.com>
2020-09-13 19:45:18 +0300

    Create ConverterCommon class and MD5Convert method.

---
+++ GirKirala Backend/AW.Core/Concrete/Common/ConverterCommon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AW.Core.Concrete.Common
+{
+    public class ConverterCommon
+    {
+        private static MD5CryptoServiceProvider md5 = null;
+        public static string MD5Convert(string text)
+        {
+            md5 = new MD5CryptoServiceProvider();
+            byte[] array = Encoding.UTF8.GetBytes(text);
+            array = md5.ComputeHash(array);
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in array)
+            {
+                sb.Append(item.ToString("x2").ToLower());
+            }
+            return sb.ToString();
+        }
+    }
+}
